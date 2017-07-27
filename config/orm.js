eea00079a727e91e0474b472c17e2c514e60ba53
msa0683 <msa0683@yahoo.com>
2017-07-27 00:01:27 -0700

    method to execute  MySQL commands created

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,25 @@
 var connection = require("../config/connection.js");
 
+//Helper function: 
+function printQquestionMarks(num) {
+	var arr = [];
 
+	for (var i = 0; i < arr.length; i++) {
+		arr.push("?");
+	}
+	return arr.toString();
+};
+
+function objToSql(ob) {
+	var arr = [];
+
+	for (var key in ob) {
+		if (Object.hasOwnProperty.call(ob, key)) {
+			arr.push(key + " = " + ob[key]);
+		}	
+	}
+	return arr.toString();
+};
 
 
 var orm = {
@@ -15,17 +34,52 @@ var orm = {
 	};
 
 	insertOne: function(table, col, val, cb) {
-		var queryString = "INSERT INTO " + 
+		var queryString = "INSERT INTO " + table;
+
+		queryString += " (";
+		queryString += cols.toString();
+		queryString += ") ";
+		queryString += "VALUES (";
+		queryString + = printQquestionMarks(vals.length);
+		queryString += ")" ;
+
+		console.log(queryString);
+		connection.query(queryString, vals, function(err, res) {
+			if (err) throw err;
+			cb(result);
+		})
 	}
 
-	updateOne: function() {
+	updateOne: function(table, objColVals, conditions, cd) {
+		var queryString = "UPDATE " + table;
 
-	}
+		queryString += " SET ";
+		queryString += objToSql(objColVals);
+		queryString += " WHERE ";
+		queryString += condition;
 
+		console.log(queryString);
+		connection.query(queryString, function(err, result) {
+			if (err) throw err
+			cd(result);
+		});
+	};
 
+	delete: function(table, condition, cb) {
+    var queryString = "DELETE FROM " + table;
+    queryString += " WHERE ";
+    queryString += condition;
+
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  }
 
 };
 
 module.exports = orm;
-
 
