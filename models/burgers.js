8e7375b2d338dc8647a86781b3cf504181b057c5
msa0683 <msa0683@yahoo.com>
2017-07-27 00:10:53 -0700

    burgers file complete

--- models/burgers.js
+++ models/burgers.js
@@ -3,3 +3,31 @@ var orm = require("../config/orm.js");
     // * Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
 
 
+var burgers = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (burgersController.js).
+module.exports = burgers;
+orm.all("burgers", cb)
+burgers.all(cb)
