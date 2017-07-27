e250b63b002b77bcef669804789f52ddfd271fb6
msa0683 <msa0683@yahoo.com>
2017-07-26 19:16:02 -0700

    mysql queries entered into schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db(
+id INT(11) NOT NULL AUTO_INCREMENT,
+burger_name Varchar(200) NOT NULL,
+devoured Boolean DEFAULT false, 
+created_at DATETIME NOT NULL,
+Primary key (id)
+);
+
