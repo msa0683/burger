ac0b48dd90ee49e4775c4e0567c06ffa441c15ac
msa0683 <msa0683@yahoo.com>
2017-07-27 17:50:05 -0700

    routers created and router exported at end of file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,12 +1,11 @@
 var burgers = require("../models/burgers.js");
 
 var express = require("express");
-var app = express();
 
 var router = express.Router();
 
 
-router.get("/", function(req,res) {
+router.get("/", function(req, res) {
 	burgers.selectAll(function(data) {
 		var hbsObject = {
 			burgers: data
@@ -16,7 +15,35 @@ router.get("/", function(req,res) {
 	});
 });
 
+router.post("/", function(req, res) {
+	burgers.create([
+		"burger_name"
+	], [
+	req.body.burger_name
+	], function() {
+		res.redirect("/");
+	});
+});
 
+router.put("/:id", function(req, res) {
+	var condition= "id =" + req.params.id;
+	console.log("condition", condition);
+
+	burgers.update({
+	burger_name: req.body.burger_name
+	}, condition, function() {
+		res.redirect("/");
+	});
+});
+
+router.delete("/:id", function(req, res) {
+	var condition = "id = " + req.params.id;
+	burgers.delete(condition, function() {
+		res.redirect("/");
+	});
+});
+
+module.exports = router; 
 
 
 
--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,1 @@
-DROP DATABASE IF EXISTS burgers_db;
-
-CREATE DATABASE burgers_db;
-
-USE burgers_db;
-
-CREATE TABLE burgers (
-id INT(11) NOT NULL AUTO_INCREMENT,
-burger_name Varchar(200) NOT NULL,
-devoured Boolean DEFAULT false, 
-created_at DATETIME NOT NULL,
-Primary key (id)
-);
 
